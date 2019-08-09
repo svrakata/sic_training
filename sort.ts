16ec9a837b61dd8cd93f7f25e720de408e604729
svrakata <svrakata@gmail.com>
2019-08-09 12:55:52 +0300

    comparer added to the implementation

--- sort.ts
+++ sort.ts
@@ -1,5 +1,7 @@
 type Comparer<T> = (arg1: T, arg2: T) => number
 type Sort<T> = (arr: T[], callback: Comparer<T>) => T[]
+type Partiotion<T> = (arr: T[], left: number, right: number) => number
+type QuickSort<T> = (arr: T[], left: number, right: number) => void
 
 type Obj = {
     a: number
@@ -8,46 +10,45 @@ type Obj = {
 const arr1 = [{ a: 88 }, { a: 777 }, { a: 567 }, { a: 86 }, { a: 11 }, { a: 3 }, { a: 13 }, { a: 23 }, { a: 5 }]
 
 const sort: Sort<Obj> = (arr: Obj[], comparer: Comparer<Obj>): Obj[] => {
+
     // the comparer is taking the same type and returns number < 0; > 0; == 0;
-    // so far the comparer is used to deside if the sorting is descendin ascending
 
     // quicksort again
     // let's go
 
-    // we have ---> recursion
     // partition ---> returns the index of the pivot
+    // we have ---> recursion with 2^n complexity
 
-    const partition = (arr: Obj[], left: number, right: number) => {
-        const pivot = arr[left].a
+    const partition: Partiotion<Obj> = (arr: Obj[], left: number, right: number) => {
+        const pivot = arr[left]
         let l = left + 1
         let r = right
         while(l <= r) {
 
-            if (arr[l].a > pivot && arr[r].a < pivot) {
+            if (comparer(arr[l], pivot) > 0 && comparer(arr[r], pivot) < 0) {
                 // swap
                 const temp = arr[l]
                 arr[l] = arr[r]
                 arr[r] = temp
             }
 
-            if (arr[l].a <= pivot) {
+            if (comparer(arr[l], pivot) <= 0) {
                 l++
             }
 
-            if(arr[r].a >= pivot) {
+            if(comparer(arr[r], pivot) >= 0) {
                 r--
             }
         }
 
         // swap pivot
-        const temp = arr[left]
         arr[left] = arr[l - 1]
-        arr[l - 1] = temp
+        arr[l - 1] = pivot
         
         return l - 1
     }
 
-    const quickSort = (arr: Obj[], left: number, right: number) => {
+    const quickSort: QuickSort<Obj> = (arr: Obj[], left: number, right: number) => {
         if (left < right) {
             let i = partition(arr, left, right)
             quickSort(arr, left, i - 1)
@@ -59,6 +60,4 @@ const sort: Sort<Obj> = (arr: Obj[], comparer: Comparer<Obj>): Obj[] => {
     return arr
 }
 
-
-// the idea is the comparer to opeate on the type of data you are sorting and to return number 
-console.log(sort(arr1, (a: Obj, b: Obj) => a.a - b.a));
+console.log(sort(arr1, (a: Obj, b: Obj) => a.a - b.a))
